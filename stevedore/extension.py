abeae9ef53930e9e05b8712e42921efbb682b5a3
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-06-19 15:53:23 -0400

    add EnabledExtensionManager and NamedExtensionManager

--- stevedore/extension.py
+++ stevedore/extension.py
@@ -26,18 +26,26 @@ class ExtensionManager(object):
         for ep in pkg_resources.iter_entry_points(self.namespace):
             LOG.debug('found extension %r', ep.name)
             try:
-                plugin = ep.load()
-                if invoke_on_load:
-                    obj = plugin(ep.name, *invoke_args, **invoke_kwds)
-                else:
-                    obj = None
+                ext = self._load_one_plugin(ep,
+                                            invoke_on_load,
+                                            invoke_args,
+                                            invoke_kwds,
+                                            )
+                if ext:
+                    self.extensions.append(ext)
             except KeyboardInterrupt:
                 raise
             except Exception as err:
-                LOG.error('Could not load %s: %s', ep.name, err)
-            else:
-                self.extensions.append(Extension(ep.name, ep, plugin, obj))
+                LOG.error('Could not load %r: %s', ep.name, err)
         return
+
+    def _load_one_plugin(self, ep, invoke_on_load, invoke_args, invoke_kwds):
+        plugin = ep.load()
+        if invoke_on_load:
+            obj = plugin(ep.name, *invoke_args, **invoke_kwds)
+        else:
+            obj = None
+        return Extension(ep.name, ep, plugin, obj)
 
     def names(self):
         "Returns the names of the discovered extensions"
@@ -68,8 +76,49 @@ class ExtensionManager(object):
             try:
                 response.append(func(e, *args, **kwds))
             except Exception as err:
-                LOG.error('error calling %s: %s', e.name, err)
+                LOG.error('error calling %r: %s', e.name, err)
         return response
 
     def __iter__(self):
         return iter(self.extensions)
+
+
+class EnabledExtensionManager(ExtensionManager):
+    """An ExtensionManager that only loads plugins that pass a check function.
+    """
+
+    def __init__(self, namespace, check_func, invoke_on_load=False,
+                 invoke_args=(), invoke_kwds={}):
+        self.check_func = check_func
+        super(EnabledExtensionManager, self).__init__(namespace,
+                                                      invoke_on_load=invoke_on_load,
+                                                      invoke_args=invoke_args,
+                                                      invoke_kwds=invoke_kwds,
+                                                      )
+
+    def _load_one_plugin(self, ep, invoke_on_load, invoke_args, invoke_kwds):
+        if not self.check_func(ep):
+            LOG.debug('ignoring extension %r', ep.name)
+            return None
+        return super(EnabledExtensionManager, self)._load_one_plugin(
+            ep, invoke_on_load, invoke_args, invoke_kwds,
+            )
+
+
+class NamedExtensionManager(EnabledExtensionManager):
+    """ExtensionManager that only loads the named extensions.
+
+    This is useful for explictly enabling extensions in a
+    configuration file, for example.
+    """
+
+    def __init__(self, namespace, names=[],
+                 invoke_on_load=False, invoke_args=(), invoke_kwds={}):
+        def check(ep):
+            return ep.name in names
+        super(NamedExtensionManager, self).__init__(namespace,
+                                                    check,
+                                                    invoke_on_load=invoke_on_load,
+                                                    invoke_args=invoke_args,
+                                                    invoke_kwds=invoke_kwds,
+                                                    )
