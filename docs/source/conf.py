7e967386c491bc8f75e7d02bfe89da60ac5bfb80
Doug Hellmann <doug.hellmann@dreamhost.com>
2013-06-03 09:53:34 -0400

    Set up extlinks extension
    Configure the extlinks extension to make it easy to link
    to issues from the documentation.
    
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,12 @@ import subprocess
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.graphviz']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.extlinks',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -242,3 +247,7 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+extlinks = {
+    'issue': ('https://github.com/dreamhost/stevedore/issues/%s', 'issue '),
+}
