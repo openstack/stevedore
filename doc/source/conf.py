a322d615dbd83cf9543589639effd9673a61c42a
Doug Hellmann <doug@doughellmann.com>
2014-10-20 16:01:36 -0400

    Bring doc build up to standard
    Move the requirements definitions and documentation files to the
    standard places used by other OpenStack projects so our doc publishing
    jobs will work.
    
    Change-Id: Iea630d827976fe517afacf4e373b3a125efff9c9

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.graphviz',
     'sphinx.ext.extlinks',
+    'oslosphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -99,7 +100,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -128,7 +129,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
