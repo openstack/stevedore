52dfe21ade9746b62393fc6ebf9b157563778a8e
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-03-09 11:40:46 +0100

    dont claim copyright for future years
    otherwise, when building from unchanged source in 2018,
    it would claim Copyright 2018, Dreamhost
    which is not true
    
    Change-Id: I5185828a965467ea04033e46a1af2435df4ad760

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'stevedore'
-copyright = u'%s, DreamHost' % datetime.date.today().year
+copyright = u'2016, DreamHost'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
